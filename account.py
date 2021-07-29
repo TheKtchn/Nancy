a0b25a6fdf5581a14a1e71c2fa45770c67e35f18
Victor Momodu <victoramomodu@gmail.com>
2021-07-29 11:34:06 +0100

    Implemented the wallet and sub-wallet multi-indexes

--- account.py
+++ account.py
@@ -26,27 +26,47 @@ class Account:
             
     # User wallets creation and management
     def user_account_data(self) -> None:
-        # pass
         try:
             data = pandas.read_csv(f"user_{self.username}_data.csv")
         except FileNotFoundError:
-            wallets = {"wallet_name": [], 
-                       "wallet_amount": []}
+            wallet_info = [[], []]
+            wallet_amounts = []
             while True:
+                wallet_names = []
                 wallet_name = input("Enter the name of the wallet: ")
-                if wallet_name == "q":
+                if wallet_name == 'q':
                     break
-                wallets["wallet_name"].append(wallet_name)
-                while True:
-                    try:
-                        wallet_amount = float(input("Enter the amount in the wallet: "))
-                    except ValueError:
-                        print("You entered a wrong value!")
-                    else:
-                        wallets["wallet_amount"].append(wallet_amount)
-                        break
-                    print("\n")
-            data = pandas.DataFrame(wallets)
-            data.to_csv(f"user_{self.username}_data.csv")
+                wallet_names.append(wallet_name)
+                sub_wallet_names = []
+                sub_wallet_amounts = []
+                if input("Does the wallet have sub-wallets(y/n)? ") == 'y':
+                    while True:
+                        sub_wallet_name = input("Enter the name of the sub-wallet: ")
+                        if (sub_wallet_name == 'q'):
+                            sub_wallet_names.append("total")
+                            sub_wallet_amounts.append(sum(sub_wallet_amounts))
+                            break
+                        sub_wallet_names.append(sub_wallet_name)
+                        wallet_names.append(wallet_name)
+                        
+                        sub_wallet_amount = float(input("Enter the amount in the sub-wallet: "))
+                        sub_wallet_amounts.append(sub_wallet_amount)
+                else:
+                    sub_wallet_names.append("total")
+                    sub_wallet_amounts.append(input("Enter the amount in the wallet: "))
+
+                wallet_info[0] += wallet_names
+                wallet_info[1] += sub_wallet_names
+                wallet_amounts += sub_wallet_amounts
+            if len(wallet_info[0]) > 0:
+                tuples = list(zip(*wallet_info))  
+                index = pandas.MultiIndex.from_tuples(tuples=tuples, names=["wallet", "sub_wallet"])
+                data = pandas.DataFrame(wallet_amounts, index=index)
+                print(data)
+                data.to_csv(f"user_{self.username}_data.csv")
         else:
             print(data)
+
+        @staticmethod
+        def error_catcher(cls):
+            pass
