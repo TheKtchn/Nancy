7877a3894b6f370c8bd834907b1f4f91eb49fd8f
Victor Momodu <victoramomodu@gmail.com>
2023-12-02 12:21:55 +0100

    Commented on Session Manager and Transaction Manager

--- session_manager.py
+++ session_manager.py
@@ -4,7 +4,23 @@ from transaction_manager import TransactionManager
 
 
 class SessionManager:
+    """
+    Manager class for handling user sessions.
+
+    This class manages the user session state and provides access to specific managers
+    (BalanceManager, BudgetManager, TransactionManager) for interacting with data related
+    to the user's financial activities.
+    """
+
     def __init__(self) -> None:
+        """
+        Initializes the SessionManager instance.
+
+        The session starts as inactive, and the user data and managers are set to None.
+
+        Returns:
+            None
+        """
         self.is_session = False
         self.user_data = None
         self.transaction_mngr = None
@@ -12,6 +28,17 @@ class SessionManager:
         self.budget_mngr = None
 
     def start_session(self, user_data):
+        """
+        Start a user session.
+
+        Activates the session and initializes managers with the provided user data.
+
+        Args:
+            user_data (dict): User data containing information about the user.
+
+        Returns:
+            None
+        """
         self.is_session = True
         self.user_data = user_data
         user_email = user_data["email"]
@@ -20,6 +47,14 @@ class SessionManager:
         self.budget_mngr = BudgetManager(user_email)
 
     def stop_session(self):
+        """
+        Stop the current user session.
+
+        Deactivates the session and sets user data and managers to None.
+
+        Returns:
+            None
+        """
         self.is_session = False
         self.user_data = None
         self.transaction_mngr = None
--- transaction_manager.py
+++ transaction_manager.py
@@ -2,25 +2,71 @@ from database import db
 
 
 class TransactionManager:
+    """
+    Manager class for interacting with the 'transactions' collection in the MongoDB database.
+    """
+
     def __init__(self, user_email) -> None:
+        """
+        Initializes the TransactionManager instance.
+
+        The 'user_id' attribute is set to the provided user email, and the 'transactions'
+        attribute is set to the 'transactions' subcollection for the specific user in the MongoDB database.
+
+        Args:
+            user_email (str): The email of the user associated with the transactions.
+
+        Returns:
+            None
+        """
         self.user_id = user_email
         self.transactions = db["transactions"][user_email]
 
     def create_transaction(self, transaction_data):
+        """
+        Creates a new transaction record in the 'transactions' collection for the associated user.
+
+        Args:
+            transaction_data (dict): The data of the transaction to be created.
+
+        Returns:
+            pymongo.results.InsertOneResult: The result of the insert operation.
+        """
         result = self.transactions.insert_one(transaction_data)
         return result
 
     def retrieve_transactions(self):
+        """
+        Retrieves all transaction records for the associated user from the 'transactions' collection.
+
+        Returns:
+            list: List of transaction records for the user.
+        """
         transaction_records = [record for record in self.transactions.find()]
         return transaction_records
 
     def delete_user_transactions(self):
-        db.drop_collection(self.transactions)
+        """
+        Deletes all transaction records for the associated user from the 'transactions' collection.
+
+        Returns:
+            pymongo.results.CollectionDeleteResult: The result of dropping the 'transactions' subcollection.
+        """
+        return db.drop_collection(self.transactions)
+
+
 
 
 if __name__ == "__main__":
     transaction_mngr = TransactionManager("someemail@email.com")
-    transaction_mngr.create_transaction({"item": "some thing", "amount": 1000})
-    transaction_mngr.create_transaction({"item": "another thing", "amount": 2000})
-    print(transaction_mngr.retrieve_transactions())
-    transaction_mngr.delete_user_transactions()
+    # print("Create transactions:")
+    # transaction_mngr.create_transaction({"item": "some thing", "amount": 1000})
+    # transaction_mngr.create_transaction({"item": "another thing", "amount": 2000})
+    # print()
+
+    # print("Transactions:")
+    # print(transaction_mngr.retrieve_transactions())
+    # print()
+    
+    print("Delete user transactions:")
+    print(transaction_mngr.delete_user_transactions())
