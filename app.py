24339698d5883e897fa6c35ebb1ad935c672d427
Victor Momodu <victoramomodu@gmail.com>
2023-12-14 09:08:31 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,47 @@
-import streamlit as st
+from enum import Enum
 from typing import List
 
+import streamlit as st
 
-def form(name: str, details: List[str], submit: str):
+if "user_data" not in st.session_state.keys():
+    st.session_state.user_data = {"name": "", "email": "", "password": ""}
+
+if "is_session" not in st.session_state.keys():
+    st.session_state.is_session = False
+
+
+class Pages(Enum):
+    HOME = "Home"
+    FINANCES = "Finances"
+    BUDGET = "Budget"
+    CHAT = "Chat"
+
+
+if "current_page" not in st.session_state.keys():
+    st.session_state.current_page = Pages.HOME
+
+
+def form(name: str, details: List[str]):
     form_data = dict()
     with st.form(key=name, clear_on_submit=True):
         for detail in details:
-            prompt = detail.replace("_", " ")
+            prompt = f"{detail.replace('_', ' ').title()}:"
             form_data[detail] = st.text_input(prompt)
 
-        is_submitted = st.form_submit_button(submit)
+        is_submitted = st.form_submit_button(name)
         if is_submitted:
-            st.write("Gotten input!")
+            yield form_data
 
 
-st.write("# Signup")
+if st.session_state.current_page == Pages.HOME:
+    st.write("# Signup")
+    form(
+        name="Signup",
+        details=["name", "email", "password"],
+    )
 
-form(
-    name="Signup",
-    details=["name", "email", "password"],
-    submit="Signup",
-)
+    st.write("# Login")
+    form(
+        name="Login",
+        details=["existing_email", "existing_password"],
+    )
